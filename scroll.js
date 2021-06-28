062cbb4dcf6f7b4e53e79156944aa2fe71554b73
Mauricio Meza <mmezab@unal.edu.co>
2021-06-27 19:18:46 -0500

    gave up trying to make carrousel

--- scroll.js
+++ scroll.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('#header');
 const navbar = document.querySelector('.navbar');
 const logo = document.querySelector('.logo-img');
+
+//When scrolled NavBar is stickied top, header raises padding and logo lowers size 
 window.onscroll = () => {
     if (window.scrollY > 50) {
         navbar.classList.add('fixed-top');
