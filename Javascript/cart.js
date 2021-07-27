875e2c6b2de538abe730612a0262176af724ef26
Mauricio Meza <mmezab@unal.edu.co>
2021-07-27 15:57:50 -0500

    initial cart implementation

---
+++ Javascript/cart.js
@@ -0,0 +1,4 @@
+prodSelect = new Array(products.length).fill(0);
+var $pick = $("#amount-picker")
+
+console.log(prodSelect)
--- Javascript/modal.js
+++ Javascript/modal.js
@@ -1,24 +1,41 @@
 var modal = document.getElementById('prod-modal')
-var picker = document.getElementById('amount-picker')
+var modalTitle = modal.querySelector('#title-prod-modal')
+var modalSubtitle = modal.querySelector('#subtitle-prod-modal')
+var modalText = modal.querySelector('#text-prod-modal')
+var modalPrice = modal.querySelector('#price-prod-modal')
+var modalImg = modal.querySelector('#img-prod-modal')
+
+var $pick = $("#amount-picker")
+
+var addButton = document.getElementById('addButton')
+
 modal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
+  //--Button that triggered the modal
   var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
+  //--Extract info from data-bs-* attributes
   var recipient = button.getAttribute('data-bs-index')
 
+  //--Get product for this modal and set its info
   const prod = products[recipient];
-
-  var modalTitle = modal.querySelector('#title-prod-modal')
-  var modalSubtitle = modal.querySelector('#subtitle-prod-modal')
-  var modalText = modal.querySelector('#text-prod-modal')
-  var modalPrice = modal.querySelector('#price-prod-modal')
-  var modalImg = modal.querySelector('#img-prod-modal')
-
-  picker.value = 1;
 
   modalImg.src = "Assets/" + prod.imagen;
   modalTitle.textContent = prod.nombre;
   modalSubtitle.textContent = prod.presentacion;
   modalText.textContent = prod.descripcion;
   modalPrice.textContent = "Precio: $" + formatPrice(prod.precio);
+
+  //--Check amount in order and set it to picker
+  var quantity = 1;
+  if(prodSelect[recipient] != 0){
+    quantity = prodSelect[recipient]
+  }
+  $pick.val(quantity);
+
+  //--Make button add the product and its amount to cart
+  addButton.addEventListener('click', function (event){
+    prodSelect[recipient] = $pick.val();
+    console.log(prodSelect);
+  })
+
+  
 })
